b37b9d618173546451481fa7b9770371b303e192
Maxim <etirel99@gmail.com>
2023-11-28 21:12:59 +0500

    2 blocks of second page

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,3 +1,57 @@
+new Swiper('.block-container-bottom', {
+    navigation: {
+      nextEl: '.block-container-bottom__arrow-right',
+      prevEl: '.block-container-bottom__arrow-left',
+    },
+    slidesPerView: 3,
+    spaceBetween: 40,
+    slidesPerGroup: 3,
+    loop: true,
+    centredSlides: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        slidesPerGroup: 1.2
+      },
+      750: {
+        slidesPerView: 2,
+        slidesPerGroup: 2
+      },
+      1500: {
+        slidesPerView: 3,
+        slidesPerGroup: 3
+      },
+    }
+  }
+)
+
+new Swiper('.comments__list', {
+    navigation: {
+      nextEl: '.comments__arrow-right',
+      prevEl: '.comments__arrow-left',
+    },
+    slidesPerView: 3,
+    spaceBetween: 40,
+    slidesPerGroup: 3,
+    loop: true,
+    centredSlides: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        slidesPerGroup: 1.2
+      },
+      750: {
+        slidesPerView: 2,
+        slidesPerGroup: 2
+      },
+      1500: {
+        slidesPerView: 3,
+        slidesPerGroup: 3
+      },
+    }
+  }
+)
+
 document.addEventListener('DOMContentLoaded', function () {
   document.querySelectorAll('.comment__container-button').forEach((el) => {
     el.addEventListener('click', () => {
@@ -7,10 +61,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
       if (content.style.maxHeight) {
         document.querySelectorAll('.comment__text').forEach((el) => el.style.maxHeight = null)
-      }
-      else {
+        el.textContent = 'Развернуть';
+      } else {
         document.querySelectorAll('.comment__text').forEach((el) => el.style.maxHeight = null)
         content.style.maxHeight = content.scrollHeight + 'px';
+        el.textContent = 'Свернуть';
       }
     })
   })
@@ -23,8 +78,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
       if (content.style.maxHeight) {
         document.querySelectorAll('.card__description').forEach((el) => el.style.maxHeight = null)
-      }
-      else {
+      } else {
         document.querySelectorAll('.card__description').forEach((el) => el.style.maxHeight = null)
         content.style.maxHeight = content.scrollHeight + 'px';
       }
